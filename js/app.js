2b2d71dbbbd9a2dfb0d3d1e45395668702efeeda
SungHyun Yu <ysh1513@naver.com>
2019-12-16 22:36:03 +0900

    vue로 검색폼만듦
    내용이없거나 x버튼 누를시 검색폼내용 삭제

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 new Vue({
   el: '#app', //아이디가 app인 DOM에 마운팅됨
   data: {
-    msg: 'hello world'
+    query:'', //query는 입력데이터. v-model='query로 연결
+  },
+  methods: {
+    onSubmit(e){//돔과 바인딩할 함수를 설정하는 곳.
+      
+    },
+    onReset(){
+      this.query='' //여기서 this는 뷰 인스턴스 = 데이터의 쿼리.
+    },
+    onKeyup(){
+      if(!this.query.length) this.onReset()
+    }
   }
 })
