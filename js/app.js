dd23e86261d1a0298e9fd70ba5de04b496a13907
SungHyun Yu <ysh1513@naver.com>
2019-12-16 23:07:43 +0900

    검색결과구현
    검색하면 임의의 결과가 나오고 x를 누르면 다 사라짐(ux)

--- js/app.js
+++ js/app.js
@@ -1,17 +1,32 @@
+import SearchModel from './models/SearchModel.js'
+
 new Vue({
   el: '#app', //아이디가 app인 DOM에 마운팅됨
   data: {
     query:'', //query는 입력데이터. v-model='query로 연결
+    submitted: false,
+    searchResult:[]
   },
   methods: {
     onSubmit(e){//돔과 바인딩할 함수를 설정하는 곳.
-      
+      this.search()
     },
     onReset(){
-      this.query='' //여기서 this는 뷰 인스턴스 = 데이터의 쿼리.
+      this.resetForm()
     },
     onKeyup(){
       if(!this.query.length) this.onReset()
+    },
+    search(){
+      SearchModel.list().then(data=>{
+        this.submitted=true
+        this.searchResult = data
+      })
+    },
+    resetForm(){
+      this.query='' //여기서 this는 뷰 인스턴스 = 데이터의 쿼리.
+      this.submitted = false
+      this.searchResult = []
     }
   }
 })
