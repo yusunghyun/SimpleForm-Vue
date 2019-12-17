e5f493e1f214abeb822774e33d15148efaca6a3a
SungHyun Yu <ysh1513@naver.com>
2019-12-17 20:25:07 +0900

    탭만듦

--- js/app.js
+++ js/app.js
@@ -5,17 +5,25 @@ new Vue({
   data: {
     query:'', //query는 입력데이터. v-model='query로 연결
     submitted: false,
+    tabs:['추천 검색어','최근 검색어'],
+    selectedTab:'',
     searchResult:[]
+  },
+  created(){//라이프사이클 : 뷰인스턴스가 생성될때 호출되는 함수
+    this.selectedTab = this.tabs[0]
   },
   methods: {
     onSubmit(e){//돔과 바인딩할 함수를 설정하는 곳.
       this.search()
     },
-    onReset(){
+    onKeyup(e){
+      if(!this.query.length) this.onReset()
+    },
+    onReset(e){
       this.resetForm()
     },
-    onKeyup(){
-      if(!this.query.length) this.onReset()
+    onClickTab(tab){
+      this.selectedTab = tab
     },
     search(){
       SearchModel.list().then(data=>{
@@ -27,6 +35,6 @@ new Vue({
       this.query='' //여기서 this는 뷰 인스턴스 = 데이터의 쿼리.
       this.submitted = false
       this.searchResult = []
-    }
+    },
   }
 })
