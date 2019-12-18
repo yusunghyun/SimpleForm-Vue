3998430f30bd7a46ce275a55a235eb720761553e
SungHyun Yu <ysh1513@naver.com>
2019-12-18 20:05:43 +0900

    추천검색어구현!

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 import SearchModel from './models/SearchModel.js'
+import KeywordModel from './models/KeywordModel.js'
 
 new Vue({
   el: '#app', //아이디가 app인 DOM에 마운팅됨
@@ -7,10 +8,12 @@ new Vue({
     submitted: false,
     tabs:['추천 검색어','최근 검색어'],
     selectedTab:'',
+    keywords:[],
     searchResult:[]
   },
   created(){//라이프사이클 : 뷰인스턴스가 생성될때 호출되는 함수
     this.selectedTab = this.tabs[0]
+    this.fetchKeyword()//뷰인스텐스가 처음으로 실행될때 가져오도록
   },
   methods: {
     onSubmit(e){//돔과 바인딩할 함수를 설정하는 곳.
@@ -24,6 +27,15 @@ new Vue({
     },
     onClickTab(tab){
       this.selectedTab = tab
+    },
+    onClickKeyword(keyword){
+      this.query = keyword
+      this.search()
+    },
+    fetchKeyword(){
+      KeywordModel.list().then(data=>{
+        this.keywords = data
+      })
     },
     search(){
       SearchModel.list().then(data=>{
